5d9be167a4d47fa7257cb4ea13448836e95e4822
= <=>
2024-06-26 10:07:29 +0100

    Installed and initialized the firebase suite

--- App.js
+++ App.js
@@ -31,10 +31,49 @@ import VerificationScreen from "./screens/auth/verificationScreen";
 import * as ExpoSplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import Personals from './screens/auth/personals';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { connectAuthEmulator, getAuth, getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
 
 ExpoSplashScreen.preventAutoHideAsync();
-
 LogBox.ignoreAllLogs();
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOcbfzkzZa_iYgwxAuAdYGtbKGd8RZYTw",
+  authDomain: "chopnow-e9949.firebaseapp.com",
+  projectId: "chopnow-e9949",
+  storageBucket: "chopnow-e9949.appspot.com",
+  messagingSenderId: "951232422686",
+  appId: "1:951232422686:web:8f4864c4ee726aab95df0f",
+  measurementId: "G-7Y2WQHXVCP"
+};
+
+const origin = "192.168.248.174";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = initializeAuth(app, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
+connectAuthEmulator(auth, `http://${origin}:9099`);
+
+const db = getFirestore(app);
+connectFirestoreEmulator(db, origin, 8080);
+
+const functions = getFunctions(app);
+connectFunctionsEmulator(functions, origin, 5001);
+
+const storage = getStorage(app);
+connectStorageEmulator(storage, origin, 9199);
+
+export { auth, db, functions, storage };
+
 
 const Stack = createStackNavigator();
 
